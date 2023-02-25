48b745f92a2c69b296ff40605165b04e1962c808
Veronica Aguirre <veroaguire@gmail.com>
2023-02-25 10:48:28 -0300

    Primer escritura del trabajo practico Front end en el repositorio

---
+++ js/function.js
@@ -0,0 +1,5 @@
+function topFunction() {
+    document.getElementById("myBtn").innerHTML
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
